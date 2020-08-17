a5a0bb668fa06c6b17592fcb1334399505e9aa60
Apurva Shukla <apurvashukla123@gmail.com>
2020-08-17 17:46:09 +1000

    Instant page added to header

--- src/components/layout.js
+++ src/components/layout.js
@@ -64,6 +64,7 @@ class Layout extends React.Component {
           }}
         >
           <header>{header}</header>
+          <script src="//instant.page/5.1.0" type="module" integrity="sha384-by67kQnR+pyfy8yWP4kPO12fHKRLHZPfEsiSXR8u2IKcTdxD805MGUXBzVPnkLHw"></script>
           <main>{children}</main>
         </div>
         <Footer>
