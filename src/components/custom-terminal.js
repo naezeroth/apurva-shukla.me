4d9553cf1f2933f968d52ba03cf7c62bd9db7e89
Apurva Shukla <apurvashukla123@gmail.com>
2020-08-19 23:40:15 +1000

    Refactored code and played with terminal a bit more

---
+++ src/components/custom-terminal.js
@@ -0,0 +1,76 @@
+import React from "react"
+import Terminal from 'terminal-in-react';
+import styled from "styled-components"
+import { rhythm } from "../utils/typography"
+
+class CustomTerminal extends React.Component {
+  componentDidMount() {
+    console.log("TERMINAL IS MOUNTED");
+  }
+  render() {
+    return (
+      <TerminalWrapper>
+        <Terminal
+          startState='maximised'
+          promptSymbol="$"
+          allowTabs={false}
+          outputColor='#93a1a1'
+          // hideTopBar={true}
+          // showActions ={false} //{False} 
+          prompt='#d33682'
+          color='#657b83'
+          backgroundColor='#073642'
+          barColor='black'
+          style={{ fontWeight: "bold", fontSize: "1.5em",  }}
+          commandPassThrough={(cmd, print) => {
+            console.log(cmd);
+            // do something async
+            // if(cmd[0] === 'blog'){
+            //   console.log('HELOO BLOG????')
+            // }
+            print(`-PassedThrough:${cmd}: command not found`);
+          }}
+          commands={{
+            // 'help': () => {return('')},
+            'blog': () => window.open('/blog', "_self"),
+            // 'open-google': () => window.open('/blog', "_self"),
+            'test': () => {return("TESTING")}, //console.log("TESTING"),
+            'showmsg': console.log('Hello World'),
+            popup: () => alert('Terminal in React')
+          }}
+          descriptions={{
+            'blog': 'see my blog',
+            showmsg: 'shows a message',
+            alert: 'alert', 
+            popup: 'alert',
+            show: false,
+            help: false,
+            clear: false,
+          }}
+          msg='Hello, my name is Apurva Shukla. Here you can find my blog, photographs and projects. Type help to get started.'
+        />
+      </TerminalWrapper>
+    )
+  }
+}
+
+//Some specific CSS modifications to Terminal
+const TerminalWrapper = styled.div`
+  .terminal-base .sc-bxivhb { 
+    max-width: 100% !important;
+    height: ${rhythm(1.2)} !important;
+    border-radius: 20px 20px 0px 0px;
+  }
+  .terminal-base .sc-EHOje {
+    max-width: 100% !important;
+    overflow-y: auto !important;
+    height: ${rhythm(20)} !important;
+    border-radius: 0px 0px 20px 20px;
+    overflow: hidden;
+  }
+  .sc-dnqmqq {
+    padding: 0px 10px 0px 0px;
+  }
+`
+
+export default CustomTerminal
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,15 +4,11 @@ import { graphql, Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Button from "../components/button"
-import Terminal from 'terminal-in-react';
-import styled from "styled-components"
-import { rhythm, scale } from "../utils/typography"
-
+import CustomTerminal from "../components/custom-terminal"
 
 class IndexPage extends React.Component {
   render() {
     console.log(this.props);
-    // console.log("QUERY IS", query);
     const { data } = this.props //I'm assuming the query is populating the this.props, the {data} is extracting data variable inside this.props
     const siteTitle = data.site.siteMetadata.title;
     console.log(data);
@@ -35,34 +31,7 @@ class IndexPage extends React.Component {
           default starter blog running on Netlify CMS.
         </p>
         <p>Now go build something great!</p> */}
-        <TerminalWrapper>
-          <Terminal
-            startState='maximised'
-            promptSymbol="$"
-            allowTabs={false}
-            outputColor='#93a1a1'
-            // hideTopBar={true}
-            // showActions ={false} //{False} 
-            prompt='#d33682'
-            color='#657b83'
-            backgroundColor='#073642'
-            barColor='black'
-            style={{ fontWeight: "bold", fontSize: "1.5em",  }}
-            commands={{
-              'open-google': () => window.open('https://www.google.com/', '_blank'),
-              'test': () => {return("TESTING")}, //console.log("TESTING"),
-              'showmsg': console.log('Hello World'),
-              popup: () => alert('Terminal in React')
-            }}
-            descriptions={{
-              'open-google': 'opens google.com',
-              showmsg: 'shows a message',
-              alert: 'alert', popup: 'alert',
-              show: false,
-            }}
-            msg='You can write anything here. Example - Hello! My name is Foo and I like Bar.'
-          />
-        </TerminalWrapper>
+        <CustomTerminal/>
         <Link to="/blog/">
           <Button marginTop="35px">Go to Blog</Button>
         </Link>
@@ -80,24 +49,6 @@ export const query = graphql`
         author
       }
     }
-  }
-`
-
-//Some specific CSS modifications to Terminal
-const TerminalWrapper = styled.div`
-  .terminal-base .sc-bxivhb { 
-    max-width: 100% !important;
-    height: ${rhythm(1.2)} !important;
-    border-radius: 20px 20px 0px 0px;
-  }
-  .terminal-base .sc-EHOje {
-    max-width: 100% !important;
-    overflow-y: auto !important;
-    height: ${rhythm(20)} !important;
-    border-radius: 0px 0px 20px 20px;
-  }
-  .sc-dnqmqq {
-    padding: 0px 10px 0px 0px;
   }
 `
 
