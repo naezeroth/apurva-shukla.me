b9ce31cb93f120c6c265abf8e2193ae7859cdf18
Apurva Shukla <apurva@air.local>
2022-11-14 23:01:27 +1100

    Fixed up blog post exports

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -9,7 +9,7 @@ import { Back } from '../components/social-icons';
 import Comments from '../components/comments';
 import { SubscribeForm } from '../components/subscribe-form';
 
-const BlogPostTemplate2 = ({
+const BlogPostTemplate = ({
     data: { mdx, site, comments },
     children,
     location,
@@ -17,7 +17,7 @@ const BlogPostTemplate2 = ({
 }) => {
     const siteTitle = site.siteMetadata.title;
     const { previous, next } = pageContext;
-    const comments2 = comments.edges;
+    const allComments = comments.edges;
 
     return (
         <Layout location={location} title={siteTitle}>
@@ -60,8 +60,6 @@ const BlogPostTemplate2 = ({
                 {mdx.frontmatter.date}
             </p>
             {children}
-            {/* {post.body} */}
-            {/* <MDXRenderer>{post.body}</MDXRenderer> */}
             <hr
                 style={{
                     marginBottom: rhythm(1),
@@ -104,118 +102,11 @@ const BlogPostTemplate2 = ({
                     marginBottom: rhythm(1),
                 }}
             />
-            <Comments slug={pageContext.slug} comments={comments2} />
+            <Comments slug={pageContext.slug} comments={allComments} />
         </Layout>
     );
 };
-export default BlogPostTemplate2;
-
-class BlogPostTemplate extends React.Component {
-    render() {
-        console.log('this', this);
-        const post = this.props.data.mdx;
-        const siteTitle = this.props.data.site.siteMetadata.title;
-        const { previous, next } = this.props.pageContext;
-        const comments = this.props.data.comments.edges;
-
-        return (
-            <Layout location={this.props.location} title={siteTitle}>
-                <SEO
-                    title={post.frontmatter.title}
-                    description={post.frontmatter.description || post.excerpt}
-                    meta={[
-                        {
-                            property: 'og:image',
-                            content: `https://apurva-shukla.me${post.frontmatter.featuredimage.childImageSharp.original.src}`,
-                        },
-                    ]}
-                />
-                <Link to="/blog/">
-                    <div
-                        style={{
-                            textDecoration: 'none !important',
-                            boxShadow: 'none !important',
-                        }}
-                    >
-                        <Back />
-                    </div>
-                </Link>
-                <h1
-                    style={{
-                        marginTop: rhythm(1 / 2),
-                        marginBottom: rhythm(1 / 8),
-                    }}
-                >
-                    {post.frontmatter.title}
-                </h1>
-                <p
-                    style={{
-                        ...scale(-1 / 5),
-                        display: 'block',
-                        marginBottom: rhythm(1),
-                        // marginTop: rhythm(-1),
-                    }}
-                >
-                    {post.frontmatter.date}
-                </p>
-                {this.children}
-                {/* {post.body} */}
-                {/* <MDXRenderer>{post.body}</MDXRenderer> */}
-                <hr
-                    style={{
-                        marginBottom: rhythm(1),
-                    }}
-                />
-                <SubscribeForm />
-                <hr
-                    style={{
-                        marginBottom: rhythm(1),
-                    }}
-                />
-                <Bio />
-                <br></br>
-                <ul
-                    style={{
-                        display: 'flex',
-                        flexWrap: 'wrap',
-                        justifyContent: 'space-between',
-                        listStyle: 'none',
-                        padding: 0,
-                    }}
-                >
-                    <li>
-                        {previous && (
-                            <Link
-                                to={`/blog${previous.fields.slug}`}
-                                rel="prev"
-                            >
-                                ← {previous.frontmatter.title}
-                            </Link>
-                        )}
-                    </li>
-                    <li>
-                        {next && (
-                            <Link to={`/blog${next.fields.slug}`} rel="next">
-                                {next.frontmatter.title} →
-                            </Link>
-                        )}
-                    </li>
-                </ul>
-                <hr
-                    style={{
-                        marginBottom: rhythm(1),
-                    }}
-                />
-                <Comments
-                    slug={this.props.pageContext.slug}
-                    comments={comments}
-                />
-            </Layout>
-        );
-    }
-}
-
-// export default BlogPostTemplate;
+export default BlogPostTemplate;
 
 export const pageQuery = graphql`
     query BlogPostBySlug($slug: String!, $slugWithoutSlash: String!) {
